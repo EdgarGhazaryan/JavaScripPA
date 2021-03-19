d1218b2d0efab5c297b7a7f1390171b8499eb2d8
EdgarGhazaryan <edgar.ghazaryan888@gmail.com>
2021-03-19 23:21:36 +0400

    add eventEmitter implementation

---
+++ src/homeworks/eventEmitterImpl/EventEmitterSingleton.js
@@ -0,0 +1,67 @@
+class EventEmitterSingleton {
+    constructor() {
+        if (!EventEmitterSingleton._instance) {
+            this._events = new Map();
+            this._maxListeners = 10; //default
+            EventEmitterSingleton._instance = this;
+        }
+        return EventEmitterSingleton._instance;
+    }
+
+    emit(eventName, ...args) {
+        if(!this._events.has(eventName)) {
+            return false;
+        }
+        this._events.get(eventName).forEach(f => f(...args));
+        return true;
+    }
+
+    on(eventName, func) {
+        if(this._events.has(eventName)) {
+            let events = this._events.get(eventName);
+            if(this._maxListeners && this._maxListeners === events.length) {
+                return;
+            }
+            events.push(func);
+        } else {
+            if(this._maxListeners !== 0) {
+                this._events.set(eventName, [func]);
+            }
+        }
+    }
+
+    off(eventName, func) {
+        if(this._events.has(eventName)) {
+            this._events.get(eventName).pop(func);
+        }
+    }
+
+    listenersCount(eventName) {
+        return this._events.has(eventName) ? this._events.get(eventName).length : 0;
+    }
+
+    listeners(eventName) {
+        return this._events.has(eventName) ? this._events.get(eventName) : [];
+    }
+
+    eventNames() {
+        let result = [];
+        for(const event of this._events.keys()) {
+            result.push(event)
+        }
+        return result;
+    }
+
+    setMaxListeners(n) {
+        if(n < 0) {
+            return;
+        }
+        this._maxListeners = n;
+    }
+
+    getMaxListeners() {
+        return this._maxListeners;
+    }
+}
+
+module.exports = EventEmitterSingleton;
